7ceb2e1878530cb9e2f854934e4386a785d381e3
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-07 17:58:04 -0600

    Clean up typos

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -3,21 +3,3 @@ class TasksController < ApplicationController
     @shelters = ['Shelter 1', 'Shelter 2', 'Shelter 3']
   end
 end
-
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/hello'
-
-helloclass Test < Minitest::Test
-def setup
-hello
-end
-
-def test_it_exists
-hello
-end
-
-def test_it_has_attributes
-hello
-end
-end
