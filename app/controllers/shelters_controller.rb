2803f9239ce536b25ea5ca692c9519afb69a5ae4
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-07 21:41:16 -0600

    Create feature to edit shelters

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -10,15 +10,36 @@ class SheltersController < ApplicationController
 
   def create
     shelter = Shelter.new({
-      name: params[:shelter][:name],
-      address: params[:shelter][:address],
-      city: params[:shelter][:city],
-      state: params[:shelter][:state],
-      zip_code: params[:shelter][:zip_code],
-      })
+                            name: params[:shelter][:name],
+                            address: params[:shelter][:address],
+                            city: params[:shelter][:city],
+                            state: params[:shelter][:state],
+                            zip_code: params[:shelter][:zip_code]
+                          })
 
     shelter.save
 
     redirect_to '/shelters'
+  end
+
+  def show
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update({
+                     name: params[:shelter][:name],
+                     address: params[:shelter][:address],
+                     city: params[:shelter][:city],
+                     state: params[:shelter][:state],
+                     zip_code: params[:shelter][:zip_code]
+                   })
+  shelter.save
+  redirect_to "/shelters/#{shelter.id}"
   end
 end
